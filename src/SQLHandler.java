54c2c9fd699d047ab6015a8a4818f0b43471b686
Mike Tee <teemh@mcmaster.ca>
2020-03-17 16:02:48 -0400

    Mike: Updated the SQLHandler. It now has a simple DFS and BFS implementation to get feel for interface and performance. Requires newer database with edgelist.

--- src/SQLHandler.java
+++ src/SQLHandler.java
@@ -4,126 +4,158 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import java.util.Set;
-import java.util.Map;
+import java.util.*;
 
-public class SQLHandler 
+public class SQLHandler
 {
+    private Connection conn = null;
+    private String path = "";
+    private Statement statement = null;
 
-  Connection conn = null;
-  String path = "";
-
-  public SQLHandler(String db_path)
-  {
-    path = db_path;
-  }
-
-  public boolean open()
-  {
-    try
+    public SQLHandler(String db_path)
     {
-      conn = DriverManager.getConnection("jdbc:sqlite:" + path);
-      return true;
+        path = db_path;
     }
-    catch(SQLException e)
-		{
-			System.err.println(e.getMessage());
-		}
-    return false;
-  }
 
-  public int getPageId(String name)
-  {
-    // select id from page where name like %name% 
-    return 0;
-  }
-
-  public Map<Integer, Map<Integer, Integer>> getGraph(int start, int end)
-  {
-    // data example:
-		/* the numbers are weights we can just set to 1
-     * this is normal data structure for dijktra.
-		graph = {'s': {'a': 2, 'b': 1},
-            'a': {'s': 3, 'b': 4, 'c':8},
-            'b': {'s': 4, 'a': 2, 'd': 2},
-            'c': {'a': 2, 'd': 7, 't': 4},
-            'd': {'b': 1, 'c': 11, 't': 5},
-            't': {'c': 3, 'd': 5}}
-		*/
-    return null;
-  }
-
-  public Set<Integer> getNeighbours(int start)
-  {
-    // set of end points from given start node
-    return null;
-  }
-
-  public Set<String> getCategories(int page_id)
-  {
-    // return set of categories for page_id
-    return null;
-  }
-
-  public boolean close()
-  {
-    try
-    {
-      if(conn != null) conn.close();
-      return true;
+    public void open() {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:" + path);
+            statement = conn.createStatement();
+            statement.setQueryTimeout(30);
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
-    catch(SQLException e)
-    {
-      System.err.println(e.getMessage());
+
+    public boolean close() {
+        try {
+            if(conn != null) conn.close();
+            return true;
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return false;
     }
-    return false;
-  }
 
-	public static void main(String[] args)
-	{
-    example();
-  }
-  
-  // EXAMPLE
-  public static void example()
-  {
-		Connection connection = null;
-		try
-		{
-			// create a database connection
-			connection = DriverManager.getConnection("jdbc:sqlite:../data/database.db3");
-			Statement statement = connection.createStatement();
-			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+    private ResultSet query(String s) throws SQLException {
+        return statement.executeQuery(s);
+    }
 
-      System.out.println("Connected");
-			
-      ResultSet rs = statement.executeQuery("select p.id, p.name from page p inner join category_page cp on p.id = cp.page_id inner join category c on c.id = cp.category_id where c.id = 1 group by p.id, p.name");
-		  
-      while(rs.next())
-			{
-				// read the result set
-				System.out.println("id = " + rs.getInt("id"));
-				System.out.println("name = " + rs.getString("name"));
-			}
-		}
-		catch(SQLException e)
-		{
-			// if the error message is "out of memory",
-			// it probably means no database file is found
-			System.err.println(e.getMessage());
-		}
-		finally
-		{
-			try
-			{
-				if(connection != null)
-					connection.close();
-			}
-			catch(SQLException e)
-			{
-				// connection close failed.
-				System.err.println(e.getMessage());
-			}
-		}
-	}
+    public String getList(int id) throws SQLException {
+        ResultSet result = query("SELECT list FROM edgelist WHERE id = " + id + ";");
+        if (!result.isClosed()){
+            return result.getString("list");
+        }
+        return null;
+    }
+
+    public int getPageId(String name) throws SQLException {
+        ResultSet result = query("SELECT id FROM page WHERE name LIKE '" + name +"' LIMIT 1;");
+        if (!result.isClosed()){
+            return result.getInt("id");
+        }
+        return -1;
+    }
+
+    public String getName(int id) throws SQLException {
+        ResultSet result = query("SELECT name FROM page WHERE id=" + id + ";");
+        if (!result.isClosed()){
+            return result.getString("name");
+        }
+        return null;
+    }
+
+    public int countEdges() throws SQLException {
+        ResultSet result = query("SELECT COUNT(*) as count FROM edgelist;");
+        if (!result.isClosed()){
+            return result.getInt("count");
+        }
+        return -1;
+    }
+
+    public ArrayList<String> getCategories(int id) throws SQLException {
+        ResultSet result = query("SELECT p.name FROM page p INNER JOIN category_page cp ON p.id = cp.page_id INNER JOIN category c ON c.id = cp.category_id WHERE c.id = " + id + " GROUP BY p.id, p.name");
+        ArrayList<String> cats = new ArrayList<>();
+        while (result.next())
+            cats.add(result.getString("name"));
+        return cats;
+    }
+
+    public ArrayList<Integer> bfs(int start, int end) throws SQLException{
+        boolean[] visited = new boolean[countEdges()];
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> path = new ArrayList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        while(!queue.isEmpty()) {
+            if (visited[end]) {
+                path.add(end);
+                return path;
+            }
+
+            int next = queue.peek();
+            queue.remove();
+
+            String list = getList(next);
+            path.add(next);
+            for (String part : list.split(" ")) {
+                int node = Integer.parseInt(part);
+
+                if (!visited[node]) {
+                    visited[node] = true;
+                    queue.add(node);
+                }
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Integer> dfs(int start, int end) throws SQLException {
+        boolean[] visited = new boolean[countEdges()];
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> path = new ArrayList<>();
+
+        stack.push(start);
+
+        while (!stack.isEmpty()){
+            int next = stack.pop();
+            if (!visited[next]) {
+                visited[next] = true;
+
+                String list = getList(next);
+                path.add(next);
+                for (String part : list.split(" ")){
+                    int node = Integer.parseInt(part);
+                    if (node == end) {
+                        path.add(end);
+                        return path;
+                    }
+                    stack.push(node);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        SQLHandler sql = new SQLHandler("database.db3");
+        sql.open();
+
+        System.out.print(sql.getPageId("Guerrilla gig") + " to ");
+        System.out.println(sql.getPageId("Antithesis"));
+
+        ArrayList<Integer> path = sql.bfs(69,64323);
+
+        for(int node : path)
+        {
+            System.out.println(sql.getName(node));
+        }
+
+        //System.out.println(sql.getName(1));
+        //System.out.println(sql.getCategories(1));
+    }
 }
