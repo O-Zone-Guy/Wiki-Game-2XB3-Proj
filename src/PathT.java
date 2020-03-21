127d22bf2c6a9fdcc692516d68f397505197eb7b
alkersho <alkersho@mcmaster.ca>
2020-03-20 21:05:07 -0400

    docs - Added comments to PathT

--- src/PathT.java
+++ src/PathT.java
@@ -1,28 +1,53 @@
+/**
+   @brief A module that provides an ADT to represent a path from one "node" to another.
+   @author Omar Alkersh
+   @file PathT.java
+   @date 2020-03-20
+ */
 
 import java.util.ArrayList;
 
+/**
+   @brief A class to represent a path from one node to another.
+ */
 public class PathT {
     private ArrayList<NodeT> path;
 
+    /**
+       @brief The constructor for PathT.
+    */
     public PathT(){
         path = new ArrayList<>();
     }
 
-	/**
-	 * @return the path
-	 */
-	public ArrayList<NodeT> getPath() {
-		return path;
-	}
+    /**
+     * @return the path
+     */
+    public ArrayList<NodeT> getPath() {
+        return path;
+    }
 
+    /**
+       @brief Add a page to the end of the path.
+       @param node∶ NodeT The node to add to the path.
+     */
     public void addPage(NodeT node){
         path.add(node);
     }
 
+    /**
+       @brief The length of the path.
+       @return The length of the path.
+     */
     public int length(){
         return path.size();
     }
 
+    /**
+       @brief Checks if any of the nodes' page names contain the string.
+       @param str∶ String The string to search for.
+       @return True if any node contains the string.
+     */
     public boolean contains(String str){
         for(NodeT node: path)
             if(node.getPageName().contains(str))
