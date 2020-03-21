5087cc02327bbaf808140d6bed4059f73cc7520f
alkersho <alkersho@mcmaster.ca>
2020-03-20 20:59:09 -0400

    docs - Added comments to NodeT

--- src/NodeT.java
+++ src/NodeT.java
@@ -1,12 +1,27 @@
+/**
+   @brief A module that provides an ADT to represent a node in the graph.
+   @file NodeT.java
+   @author Omar Alkresh - alkersho
+   @date 2020-03-20
+ */
 
-import java.util.HashSet;
+import java.util.Collection;
 
+/**
+   @brief A class to represent a node in the graph. Contains the name, number, and neighbours of the node.
+ */
 public class NodeT {
     private int id;
     private String name;
-    private HashSet<Integer> neighbours;
+    private Collection<Integer> neighbours;
 
-    public NodeT(int id, String name, HashSet<Integer> neihbours){
+    /**
+       @brief A constructor for the NodeT
+       @param id∶ int The node ID/Number
+       @param name∶ String The name of the wiki page.
+       @param neighbours∶ Collection<Integer> The collection of the IDs of the neighbours.
+     */
+    public NodeT(int id, String name, Collection<Integer> neihbours){
 
         this.id = id;
         this.name = name;
@@ -15,14 +30,14 @@ public class NodeT {
     }
 
 	/**
-	 * @return the id
+	 * @return the id of the page.
 	 */
 	public int gePageNumber() {
 		return id;
 	}
 
 	/**
-	 * @return the name
+	 * @return the name of the page
 	 */
 	public String getPageName() {
 		return name;
@@ -32,7 +47,7 @@ public class NodeT {
 	/**
 	 * @return the neighbours
 	 */
-	public HashSet<Integer> getNeighbours() {
+	public Collection<Integer> getNeighbours() {
 		return neighbours;
 	}
 
