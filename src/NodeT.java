2852f6e7182ceeb87941e0af78412c739e124aca
Mike <teemh@mcmaster.ca>
2020-03-23 21:31:23 -0400

    Created AllTests in tests folder inside src. Added rules to makefile to run tests. Changed SQLHandler to better match the specification.

--- src/NodeT.java
+++ src/NodeT.java
@@ -17,38 +17,29 @@ public class NodeT {
 
     /**
        @brief A constructor for the NodeT
-       @param id∶ int The node ID/Number
-       @param name∶ String The name of the wiki page.
-       @param neighbours∶ Collection<Integer> The collection of the IDs of the neighbours.
+       @param id: int The node ID/Number
+       @param name: String The name of the wiki page.
+       @param neighbours: Collection<Integer> The collection of the IDs of the neighbours.
      */
-    public NodeT(int id, String name, Collection<Integer> neihbours){
-
+    public NodeT(int id, String name, Collection<Integer> neighbours){
         this.id = id;
         this.name = name;
         this.neighbours = neighbours;
-
     }
 
 	/**
 	 * @return the id of the page.
 	 */
-	public int gePageNumber() {
-		return id;
-	}
+	public int gePageNumber() { return id; }
 
 	/**
 	 * @return the name of the page
 	 */
-	public String getPageName() {
-		return name;
-	}
-
+	public String getPageName() { return name; }
 
 	/**
 	 * @return the neighbours
 	 */
-	public Collection<Integer> getNeighbours() {
-		return neighbours;
-	}
+	public Collection<Integer> getNeighbours() { return neighbours; }
 
 }
