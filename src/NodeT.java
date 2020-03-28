8ba87cc0b58dc8ffc416ab0aac63f4c7ca33211e
alkersho <alkersho@mcmaster.ca>
2020-03-27 21:36:47 -0400

    refact - Refactored class and made types more general

--- src/NodeT.java
+++ src/NodeT.java
@@ -3,44 +3,47 @@
    @file NodeT.java
    @author Omar Alkresh - alkersho
    @date 2020-03-20
- */
+*/
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
    @brief A class to represent a node in the graph. Contains the name, number, and neighbours of the node.
- */
+*/
 public class NodeT {
     private int id;
     private String name;
-    private Collection<Integer> neighbours;
+    private Set<Integer> neighbours;
 
     /**
        @brief A constructor for the NodeT
        @param id: int The node ID/Number
        @param name: String The name of the wiki page.
        @param neighbours: Collection<Integer> The collection of the IDs of the neighbours.
-     */
+    */
     public NodeT(int id, String name, Collection<Integer> neighbours){
         this.id = id;
         this.name = name;
-        this.neighbours = neighbours;
+        this.neighbours = new HashSet<>(neighbours);
     }
 
-	/**
-	 * @return the id of the page.
-	 */
-	public int gePageNumber() { return id; }
+    /**
+     * @return the id of the page.
+     */
+    public int getPageNumber() { return id; }
 
-	/**
-	 * @return the name of the page
-	 */
-	public String getPageName() { return name; }
+    /**
+     * @return the name of the page
+     */
+    public String getPageName() { return name; }
 
-	/**
-	 * @return the neighbours
-	 */
-	public Collection<Integer> getNeighbours() { return neighbours; }
+    /**
+     * @return the neighbours
+     */
+    public Collection<Integer> getNeighbours() { return neighbours; }
 
     /**
        @brief Checks if two NodeT objects are equal.
@@ -52,13 +55,12 @@ public class NodeT {
             return true;
         if (! (other instanceof NodeT))
             return false;
-        NodeT node2 = (NodeT) other;
-        return this.id == node2.id;
+        return this.id == ((NodeT) other).getPageNumber();
     }
     /**
        @brief The Hash function for the node
        @return The hash, its id.
-     */
+    */
     public int hashCode(){
         return id;
     }
