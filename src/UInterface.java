703fb05f345db4e641b26864c40c66901622cdeb
Mike <teemh@mcmaster.ca>
2020-04-10 17:56:46 -0400

    MIKE: Cleaned up a user interface a bit.

--- src/UInterface.java
+++ src/UInterface.java
@@ -5,7 +5,6 @@
  @date 2020-03-22
  */
 
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
@@ -23,23 +22,13 @@ public class UInterface{
 	private JFrame frame;
 	private JTextField t1;
 	private JTextField t2;
-	String pageOne, pageTwo, pageTwo_determined;
-	int pageOneId, pageTwoId, pageTwo_determinedId;
 
 	/**
 	 * @brief Launching the application and displaying the form
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UInterface window = new UInterface();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		UInterface window = new UInterface();
+		window.frame.setVisible(true);
 	}
 
 	/**
@@ -57,25 +46,18 @@ public class UInterface{
 		// Creating a new Frame and setting basic operations
 		frame = new JFrame();
 		frame.setBounds(0, 0, 524, 240);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-
-		// MIKE: title
-		frame.setTitle("Wikigame");
-
-		//  MIKE: draws frame in center of screen
-		frame.setLocationRelativeTo(null);;
+		frame.setTitle("Wiki Game Shortest Path");
+		frame.setLocationRelativeTo(null); // draw frame in center of screen
 
 		// Creating a new text field for input for page 1
 		t1 = new JTextField();
 		t1.setBounds(96, 94, 152, 20);
 		frame.getContentPane().add(t1);
-		t1.setColumns(10);
-		t1.setText("Kleroterion");
+		t1.setText("Kleroterion"); // set example text
 
 		// Creating a new text field for input for page 2
 		t2 = new JTextField();
-		t2.setColumns(10);
 		t2.setBounds(286, 94, 152, 20);
 		frame.getContentPane().add(t2);
 		t2.setText("Pinakion");
@@ -93,48 +75,46 @@ public class UInterface{
 		frame.getContentPane().add(l2);
 
 		// Example Text
-		JLabel lblNewLabel_1 = new JLabel("Example ");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(0, 24, 100, 14);
-		frame.getContentPane().add(lblNewLabel_1);
+		JLabel example_label = new JLabel("Example ");
+		example_label.setHorizontalAlignment(SwingConstants.CENTER);
+		example_label.setBounds(0, 24, 100, 14);
+		frame.getContentPane().add(example_label);
 
 		// Custom Text
-		JLabel lblNewLabel_2 = new JLabel("Custom ");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(0, 96, 100, 14);
-		frame.getContentPane().add(lblNewLabel_2);
+		JLabel custom_label = new JLabel("Custom ");
+		custom_label.setHorizontalAlignment(SwingConstants.CENTER);
+		custom_label.setBounds(0, 96, 100, 14);
+		frame.getContentPane().add(custom_label);
 
 		// Creating a combobox for a predetermined destination
-		String[] start_examples = new String[] {"", "Glover, Missouri TO Doss, Missouri"};
+		String[] start_examples = new String[] {"CUSTOM",
+				"Glover, Missouri TO Doss, Missouri",
+				"Lee's Summit, Missouri TO Missouri Route 350",
+		};
 		JComboBox<String> c1 = new JComboBox<>(start_examples);
-		c1.setBounds(96, 24, 348, 20);
+		c1.setBounds(96, 24, 341, 20);
 		frame.getContentPane().add(c1);
 
 		// Creating a new button for execution
-		JButton btnNewButton = new JButton("Find Paths!");
+		JButton button = new JButton("");
+		button.setText("Find Paths!");
 		// Adding an ActionListener to the button for it to access ButtonClicked functionality
-		btnNewButton.addActionListener(new ActionListener() {
+		button.addActionListener(new ActionListener() {
 			/**
 			 * @brief Retrieves data from two text fields and performs functions
 			 * @param e Button Clicked ActionEvent
 			 */
 			public void actionPerformed(ActionEvent e) {
-				String start = "";
-				String end = "";
+
+				String start = t1.getText().trim();
+				String end = t2.getText().trim();
 
 				String combo_value = String.valueOf(c1.getSelectedItem()).trim();
-				System.out.println(combo_value);
-				if (!combo_value.equals(""))
+				if (!combo_value.equals("CUSTOM"))
 				{
 					String[] split = combo_value.split(" TO ");
 					start = split[0];
 					end = split[1];
-					System.out.println(start + "" + end);
-				}
-				else
-				{
-					start = t1.getText().trim();
-					end = t2.getText().trim();
 				}
 
 				int start_id = 0;
@@ -155,38 +135,42 @@ public class UInterface{
 				}
 
 				try {
-					runSearch(start_id, end_id);
+					run(start_id, end_id);
 				} catch (SQLException ex) {
-					ex.printStackTrace();
+					//ex.printStackTrace();
+					JOptionPane.showMessageDialog(null, "SQL Error");
 				}
+
+				button.setText("Find Paths!");
 			}
 		});
-		btnNewButton.setBounds(190, 140, 148, 36);
-		frame.getContentPane().add(btnNewButton);
+		button.setBounds(190, 140, 148, 36);
+		frame.getContentPane().add(button);
 
 	}
 
-	public void runSearch(int start, int end) throws SQLException {
-		// MIKE: create new frame for each result.
+	public void run(int start, int end) throws SQLException {
+		// create new frame for each result.
 		Set<PathT> result = Algorithms.getPaths(start, end);
 		NodeT first = SQLHandler.getNode(start);
 
 		String start_name = SQLHandler.getPageName(start);
 		String end_name = SQLHandler.getPageName(end);
 
-		if (result.size()==0) {
+		if (result.size() == 0) {
 			JOptionPane.showMessageDialog(null, "There is no direct path from " + start_name + " to " + end_name);
+			return;
 		}
-		else {
-			for (PathT path : result)
-			{
-				ArrayList<NodeT> nodes = new ArrayList<>();
-				nodes.add(first);
-				nodes.addAll(path.getPath());
 
-				UIResults pt = new UIResults(nodes);
-			}
+		for (PathT path : result)
+		{
+			ArrayList<NodeT> nodes = new ArrayList<>();
+			nodes.add(first);
+			nodes.addAll(path.getPath());
+
+			UIResults pt = new UIResults(nodes);
 		}
 	}
 }
+
 
