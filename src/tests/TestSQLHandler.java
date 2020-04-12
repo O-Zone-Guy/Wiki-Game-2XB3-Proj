f89763bf41eba7f2005ba53a9581e86fec3eab67
alkersho <alkersho@mcmaster.ca>
2020-04-11 22:08:15 -0400

    test - Added SQLHandler tests

--- src/tests/TestSQLHandler.java
+++ src/tests/TestSQLHandler.java
@@ -1,14 +1,22 @@
 package tests;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import cas.xb3.proj.alg.SQLHandler;
 
 public class TestSQLHandler {
 
-  @Test
-  public void test()
-  {
-    assert(1 == 1);
-  }
+    @Test
+    public void getName() throws SQLException{
+        assertEquals("Chiasmal syndrome", SQLHandler.getPageName(0));
+    }
 
+    @Test
+    public void getID() throws SQLException{
+        assertEquals(0, SQLHandler.getPageId("Chiasmal syndrome"));
+    }
 }
